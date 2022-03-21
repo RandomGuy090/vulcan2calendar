f41cf1c9bb1208eec94dc1bf29a0119e90c0e8e3
RandomGuy090 <randomguy0090@gmail.com>
2022-03-22 00:50:43 +0100

    reformatted code, two different loops (homework and exams), homework event added to google calendar

--- vulcan2calendar.py
+++ vulcan2calendar.py
@@ -14,27 +14,68 @@ from time import time, strftime, sleep
 from google_calendar import Google_user
 from vulcan_user import Vulcan_user
 
+all_events = []
+
+google = Google_user()
+service = google.get_build()
+
+calendar_obj = google.calendar_handler()
+
+client = Vulcan_user()
+
+def parse_google(elem):
+	for elem in all_events:
+		google.create_event(body=elem)
+		# print(elem)
+		pass
+
+async def homework_loop(client):
+	await client.set_student()
+
+	while True:
+		homework = await client.get_homework()
+
+		async for work in homework:
+			if work.deadline.date_time > datetime.datetime.now():
+
+				event = {
+							'summary': work.subject.name,
+							'description': work.content,
+							'start': {
+								'dateTime': f"{str(work.deadline.date)}T00:00:00",
+								'timeZone': 'Europe/Warsaw'
+
+							},
+							'end': {
+								'dateTime': f"{str(work.deadline.date)}T00:00:00",
+								
+								'timeZone': 'Europe/Warsaw'
+							},
+							'reminders': {
+							'useDefault': True,
+							},
+							 'reminders': {
+						    'useDefault': False,
+						    'overrides': [
+						      {'method': 'popup', 'minutes': 15*60},
+						    ],
+						  },
+						}
+					# print(event)
+				all_events.append(event)
+				parse_google(event)
 
 
 
+	sleep(5)
 
-async def main(loop): 
-
-
-	google = Google_user()
-	service = google.get_build()
-
-
-	calendar_obj = google.calendar_handler()
-
-	client = Vulcan_user()
+async def exams_loop(client):
 	await client.set_student()
 
 	while True:
 
 		exams = await client.get_exams()
 
-		all_events = []
 		try:
 			async for elem in exams:
 				if elem.deadline.date_time > datetime.datetime.now():
@@ -63,16 +104,24 @@ async def main(loop):
 						}
 					# print(event)
 					all_events.append(event)
+					parse_google(event)
 			
-			for elem in all_events:
-				google.create_event(body=elem)
+			
 		
-		except:
-			print("xD")
+		except Exception as e:
+			# print(f"xD ({e})")
 			pass
 
 
 		sleep(5)
+
+# async def main(): 
+
+	
+	
+
+	# await exams_loop(client)
+	# await homework_loop(client)
 
 
 
@@ -82,4 +131,13 @@ async def main(loop):
 if __name__ == "__main__":
 
 	loop = asyncio.get_event_loop()
-	loop.run_until_complete(main(loop))
+	loop2 = asyncio.get_event_loop()
+	
+	# loop.create_task(main())
+	
+	loop2.create_task(homework_loop(client))
+	loop.create_task(exams_loop(client))
+	
+	loop.run_forever()
+	loop2.run_forever()
+	# loop.run_until_complete(main(loop))
